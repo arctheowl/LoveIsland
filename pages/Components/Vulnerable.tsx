2c3649c1f5f148568807ce143e32f95a8bfb3c5c
lidevlin <liam.devlin@iqvia.com>
2022-06-08 13:50:33 +0100

    Got rid of image

--- pages/Components/Vulnerable.tsx
+++ pages/Components/Vulnerable.tsx
@@ -1,6 +1,6 @@
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
+// import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 
@@ -8,13 +8,6 @@ export default function Vulnerable() {
   return (
     <Card className="col-span-6 text-center">
       <CardActionArea>
-        <CardMedia
-          component="img"
-          height="40"
-          image="/image/LiamVulnerable.jpeg"
-          alt="Liam"
-          className="w-1/2 lg:ml-56 md:ml-32 sm:ml-20 ml-10"
-        />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             Currently Vulnerable
